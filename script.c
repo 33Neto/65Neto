9c2314dc8d95a754b691bc940df874d543c940b6
33Neto <97692627+33Neto@users.noreply.github.com>
2022-04-09 13:47:44 -0400

    Update script.c

--- script.c
+++ script.c
@@ -2,14 +2,25 @@
 int main()
 {
     int contador;
-
- printf ("QUAL SUA IDADE?\n");
+ printf(".............................\n");    
+ printf("BEM VINDO A MINHA FERRAMENTA!\n");
+ printf(".............................\n");   
+ printf("\n \n \n \n \n \n \n");
+ 
+ printf("..............................\n");
+ printf("Criador github/33Net/65Neto!!!\n");
+ printf("..............................\n");
+    
+ printf("\n \n \n \n");
+    
+ printf ("Digite Sua Idade!\n");
 scanf ("%i",&contador);
   
   for (contador = 1; contador <= 99999;)
   {
-    printf("Eneias GAY GAY\n");
+    printf("QUEM DIGITOU E GAY KKKKKK \n");
   }
   
   return(0);
-}
+  
+
