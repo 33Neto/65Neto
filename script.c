3fc4aa6ce7d7d230b72e278f3d4752e21b886f26
33Neto <97692627+33Neto@users.noreply.github.com>
2022-04-09 00:51:31 -0400

    Add files via upload

---
+++ script.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    int contador;
+
+ printf ("QUAL SUA IDADE?\n");
+scanf ("%i",&contador);
+  
+  for (contador = 1; contador <= 99999;)
+  {
+    printf("Eneias GAY GAY\n");
+  }
+  
+  return(0);
+}
